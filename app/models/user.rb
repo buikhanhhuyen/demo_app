c0c3df5207771c700e42ed154a5b91411544914c
huyen <bui.khanh.huyen@framgia.com>
2016-08-18 15:24:48 +0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 6}, allow_nil: true
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
+  def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+    BCrypt::Password.create string, cost: cost
   end
 
   # Returns a random token.
@@ -23,17 +23,22 @@ class User < ApplicationRecord
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
+    update_attribute :remember_digest, User.digest(remember_token)
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
+  def authenticated? remember_token
     return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password? remember_token
   end
 
   # Forgets a user.
   def forget
-    update_attribute(:remember_digest, nil)
+    update_attribute :remember_digest, nil
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
   end
 end
